fb596f0510b96c2e1cbbe3e738b94ec1b958067e
Randy H <52173775+randy-halim@users.noreply.github.com>
2020-12-09 04:31:49 +0000

    Properly runs with env variable and tokens

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import { Client } from "discord.js";
+
+const Discord = new Client();
+Discord.on("ready", () => {
+  console.log("Joined!");
+});
+
+const { DISCORD_TOKEN } = process.env;
+if (!DISCORD_TOKEN || DISCORD_TOKEN === "REPLACE ME") {
+  console.error("Discord token wasn't found, aborting!");
+  process.exit(1);
+}
+Discord.login(DISCORD_TOKEN);
